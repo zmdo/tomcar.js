38741c9fff08309a4163d86197234e9a87baf9b9
zmdo <zmgithub@163.com>
2021-04-12 18:30:25 +0800

    update example

--- src/tomcar.ts
+++ src/tomcar.ts
@@ -1,21 +1,47 @@
 import WorkFlow from "./core/workflow";
 
-export async function TomCarStart(workflow:WorkFlow): Promise<void>{
+export default class TomcarController {
 
-    workflow.Init();
-    workflow.Start();
+    workflow:WorkFlow;
+    runFlag:boolean = true;
+    pauseFlag:boolean = false;
 
-    while(true) {
+    constructor(workflow:WorkFlow){
+        this.workflow = workflow;
+    }
 
-        while(!workflow.IsEnd()) {
-            workflow.NextStep();
-            await sleep(10);
+    public Begin():void {
+        this.workflow.Init();
+        this.workflow.Start();
+        this.runFlag = true;
+        this.pauseFlag = false;
+        this.TaskRun();
+    }
+
+    public Start():void {
+        this.pauseFlag = false;
+    }
+
+    public Pause():void {
+        this.pauseFlag = true;
+    }
+
+    public Stop():void {
+        this.runFlag = false;
+    }
+
+    private async TaskRun() {
+        while(this.runFlag) {
+            while((!this.workflow.IsEnd()) && this.runFlag) {
+                if (!this.pauseFlag) {
+                    this.workflow.NextStep();
+                    this.workflow.OrderdCars();
+                }
+                await sleep(10);
+            }
+            this.workflow.ExecuteStrategicPlan();
+            this.workflow.ReStart();
         }
-
-        workflow.OrderdCars();
-        workflow.ExecuteStrategicPlan();
-        workflow.ReStart();
-
     }
 }
 
